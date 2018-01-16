85f0704668e699c0a1ff6c1b4960ce22482d090e
Nelson Gustavo <nelsongustavo@Nelsons-MacBook-Pro.local>
2018-01-15 22:04:36 -0200

    First footer version

--- src/Shared/Layout/_/Footer.jsx
+++ src/Shared/Layout/_/Footer.jsx
@@ -1,12 +1,72 @@
 import React from 'react';
-import { Grid, Navbar, Row } from 'react-bootstrap';
+import { Grid, Col, Row } from 'react-bootstrap';
+
+const dummySentences = [
+	'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+	'Donec hendrerit tempor tellus.',
+	'Donec pretium posuere tellus.',
+	'Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.',
+	'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+	'Nulla posuere.',
+	'Donec vitae dolor.',
+	'Nullam tristique diam non turpis.',
+	'Cras placerat accumsan nulla.',
+	'Nullam rutrum.',
+	'Nam vestibulum accumsan nisl.'
+];
 
 export default function Footer() {
   return (
-    <Navbar inverse className="layout-footer">
-      <Grid>
-        <Row className="show-grid" />
-      </Grid>
-    </Navbar>
+    <footer className="footer">
+        <Grid>
+          <img class="logo" alt="Smart Fit" title="Smart Fit" src="http://www.fitparkacademia.com.br/wp-content/themes/ovid/data/1330435782_Logomarca_fitPark.png" />
+          <Row className="show-grid footer-menu">
+            <Col sm={6} md={3}>
+              <h3>Institucional</h3>
+            	<ul className="list">
+                <li className="item"><a href="#">teste</a></li>
+                <li className="item"><a href="#">teste</a></li>
+                <li className="item"><a href="#">teste</a></li>
+              </ul>
+            </Col>
+            <Col sm={6} md={3}>
+              <h3>Planos</h3>
+              <ul className="list">
+                <li className="item"><a href="#">teste</a></li>
+                <li className="item"><a href="#">teste</a></li>
+                <li className="item"><a href="#">teste</a></li>
+              </ul>
+            </Col>
+            <Col sm={6} md={3}>
+              <h3>Corporativo</h3>
+              <ul className="list">
+                <li className="item"><a href="#">teste</a></li>
+                <li className="item"><a href="#">teste</a></li>
+                <li className="item"><a href="#">teste</a></li>
+              </ul>
+            </Col>
+            <Col sm={6} md={3}>
+              <h3>Siga a FitPark</h3>
+              <ul className="list">
+                <li className="item"><a href="#">teste</a></li>
+                <li className="item"><a href="#">teste</a></li>
+                <li className="item"><a href="#">teste</a></li>
+              </ul>
+            </Col>
+          </Row>
+          <Row>
+            <div className="footer-info">
+              <h3>Bla Bla Bla</h3>
+              <p>
+                O preço da mensalidade não inclui taxa de adesão ou de manutenção anual.
+                Pagamento mensal por débito automático em conta corrente ou no cartão de crédito.
+                Sujeito a alterações em aviso prévio. Não aceitamos cheques. Pagamento em dinheiro
+                deve ser feito à vista, direto na unidade, incluindo mensalidades do período de 12 meses,
+                taxa de adesão e manutenção anual vigentes.
+              </p>
+            </div>
+          </Row>
+        </Grid>
+    </footer>
   );
 }
